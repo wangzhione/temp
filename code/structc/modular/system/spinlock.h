8cc59f8c46e765aac43e8660f1c2c470e9c897e1
Zhi Wang <wangzhi5@xiaomi.com>
2021-12-23 10:09:46 +0800

    structc 项目重启

---
+++ code/structc/modular/system/spinlock.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdatomic.h>
+
+// spin lock
+inline void atomic_flag_lock(volatile atomic_flag * lock) {
+    while (atomic_flag_test_and_set(lock)) {}
+}
+
+inline void atomic_flag_unlock(volatile atomic_flag * lock) {
+    atomic_flag_clear(lock);
+}
+
+inline _Bool atomic_flag_trylock(volatile atomic_flag * lock) {
+    return atomic_flag_test_and_set(lock) == 0;
+}
