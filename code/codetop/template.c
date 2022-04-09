d222636afd1d1d2c89b64f88cf4e60cd18af6384
Zhi Wang <wangzhi5@xiaomi.com>
2022-04-09 21:03:26 +0800

    reverse k group

---
+++ code/codetop/template.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <errno.h>
+#include <wchar.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+/* describe : 
+
+   
+ */
+
+// build:
+// gcc -g -O3 -Wall -Wextra -Werror -o {name} {name}.c
+//
+int main(void) {
+
+    exit(EXIT_SUCCESS);
+}
---
+++ code/codetop/template.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ue
+
+if [[ $# != 1 ]]
+then
+    echo "usage: $0 {name}"
+    exit -1
+fi
+
+name=$1
+file=${name}.c
+
+#
+# 1. 用户输入文件名, 我们尝试创建文件
+#
+# ${file##.} -> 后缀名 suffix
+# ${file%%.} -> 文件名称
+#
+
+if [[ -a ${file} ]]
+then
+    echo "${file} exists"
+    exit 0
+fi
+
+readonly template_file=template.c
+
+sed "s/{name}/${name}/g" ${template_file} > ${file}
+
+code ${file}
