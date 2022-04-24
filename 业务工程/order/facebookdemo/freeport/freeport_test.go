5b0c017cc9d967b296b04f9c7552ae3a07f32b10
Zhi Wang <wangzhi5@xiaomi.com>
2022-04-24 17:48:31 +0800

    http 相关代码

---
+++ 业务工程/order/facebookdemo/freeport/freeport_test.go
@@ -0,0 +1,35 @@
+package freeport
+
+import (
+	"net"
+	"strconv"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	port, err := Get()
+	if err != nil {
+		t.Fatalf("Got err from Get: %+v", err)
+	}
+
+	if port == 0 {
+		t.Fatal("Got port 0")
+	}
+
+	t.Logf("port = %d", port)
+}
+
+func TestGetIsClosed(t *testing.T) {
+	port, err := Get()
+	if err != nil {
+		t.Fatalf("Got err from Get: %+v", err)
+	}
+
+	listener, err := net.Listen("tcp", "127.0.0.1:"+strconv.Itoa(port))
+	if err != nil {
+		t.Fatalf("Got err from net.Listen: %+v", err)
+	}
+	defer listener.Close()
+
+	t.Logf("net.Listen 127.0.0.1:" + strconv.Itoa(port))
+}
